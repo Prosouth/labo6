7ffa62f31c520f09820345238680b53719308104
Sebastien Saez <monsieursaez@gmail.com>
2016-11-17 15:45:05 +0100

    added header file and function file

---
+++ labo6/montantEnVaudois.cpp
@@ -0,0 +1,24 @@
+#include <string>
+
+using namespace std;
+
+/**
+ \brief Traduit des nombres réels en prix exprimés en vaudois
+ \param[in] montant un réel compris entre 0 et 999999.99 CHF.
+ \return une chaine de caractères indiquant en vaudois le prix
+ en francs et centimes.
+ \details Exemples:
+ 12.30  -> "douze francs et trente centimes"
+ 200.01 -> "deux cents francs et un centime"
+ 180    -> "cent huitante francs"
+ 1.80   -> "un franc et huitante centimes"
+ 0.20   -> "vingt centimes"
+ 0      -> "zéro franc"
+ */
+string montantEnVaudois(double montant)
+{
+   
+}
+
+
+
---
+++ labo6/montantEnVaudois.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   montantEnVaudois.h
+ * Author: prosouth
+ *
+ * Created on 17 November 2016, 15:43
+ */
+#ifndef MONTANTENVAUDOIS_H
+#define MONTANTENVAUDOIS_H
+
+string montantEnVaudois(double montant);
+
+
+#endif /* MONTANTENVAUDOIS_H */
+
