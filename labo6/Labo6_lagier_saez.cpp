87cf7902c43cb3ab3ab9ba1d7198522c2ab0c24a
Sebastien Saez <monsieursaez@gmail.com>
2016-11-24 23:16:12 +0100

    spacing ftw

--- labo6/Labo6_lagier_saez.cpp
+++ labo6/Labo6_lagier_saez.cpp
@@ -32,20 +32,19 @@ const int    MILLE = 1000,
              CENT = 100,
              DIX = 10;
 
-
 /**
-    \brief Traduit des nombres réels en prix exprimés en vaudois
-    \param[in] MONTANT un réel compris entre 0 et 999999.99 CHF.
-    \return une chaine de caractères indiquant en vaudois le prix
-    en francs et centimes.
-    \details Exemples:
-    12.30  -> "douze francs et trente centimes"
-    200.01 -> "deux cents francs et un centime"
-    180    -> "cent huitante francs"
-    1.80   -> "un franc et huitante centimes"
-    0.20   -> "vingt centimes"
-    0      -> "zéro franc"
-    */
+\brief Traduit des nombres réels en prix exprimés en vaudois
+\param[in] MONTANT un réel compris entre 0 et 999999.99 CHF.
+\return une chaine de caractères indiquant en vaudois le prix
+en francs et centimes.
+\details Exemples:
+12.30  -> "douze francs et trente centimes"
+200.01 -> "deux cents francs et un centime"
+180    -> "cent huitante francs"
+1.80   -> "un franc et huitante centimes"
+0.20   -> "vingt centimes"
+0      -> "zéro franc"
+*/
 string montantEnVaudois(const double MONTANT);
 
 
